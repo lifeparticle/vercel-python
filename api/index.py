39be8484e59e050048fd54375a3a1daf85016b48
genesis <mahbub.zaman@supporterhub.com>
2020-12-26 15:39:30 +1100

    chk req

--- api/index.py
+++ api/index.py
@@ -1,14 +1,17 @@
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
+import requests
 
 class handler(BaseHTTPRequestHandler):
 
 	def do_GET(self):
-		s = self.path
-		name = parse_qs(urlparse(s).query)['name'][0]
+		# s = self.path
+		# name = parse_qs(urlparse(s).query)['name'][0]
 		self.send_response(200)
 		self.send_header('Content-type','text/plain')
 		self.end_headers()
-		message = "Hello " + name
-		self.wfile.write(message.encode())
+		# message = "Hello " + name
+		s = self.request.GET.get('name')
+
+		self.wfile.write(s.encode())
 		return
