9f5f1e044ed215c50fc5fbffcd2e169fb79064a3
genesis <mahbub.zaman@supporterhub.com>
2020-12-26 15:13:52 +1100

    url

---
+++ api/index.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+def do_GET(self):
+	s = self.path
+	name = parse_qs(urlparse(s).query)['name'][0]
+	self.send_response(200)
+	self.send_header('Content-type','text/plain')
+	self.end_headers()
+	message = "Hello " + name
+	self.wfile.write(message.encode())
+	return
