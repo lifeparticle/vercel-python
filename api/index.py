1b28b0308964a83a8afa8ea99e730c8f98c3a06b
genesis <mahbub.zaman@supporterhub.com>
2020-12-26 15:24:15 +1100

    indent

--- api/index.py
+++ api/index.py
@@ -2,12 +2,12 @@ from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
 
-def do_GET(self):
-	s = self.path
-	name = parse_qs(urlparse(s).query)['name'][0]
-	self.send_response(200)
-	self.send_header('Content-type','text/plain')
-	self.end_headers()
-	message = "Hello " + name
-	self.wfile.write(message.encode())
-	return
+	def do_GET(self):
+		s = self.path
+		name = parse_qs(urlparse(s).query)['name'][0]
+		self.send_response(200)
+		self.send_header('Content-type','text/plain')
+		self.end_headers()
+		message = "Hello " + name
+		self.wfile.write(message.encode())
+		return
