7bdf5d2f80e34961c07b3060ae33c72699c1fbe6
rambogerbert <30780993+rambogerbert@users.noreply.github.com>
2022-09-05 15:12:23 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import requests
 
 class handler(BaseHTTPRequestHandler):
 
@@ -11,9 +12,57 @@ class handler(BaseHTTPRequestHandler):
 		self.end_headers()
 
 		if "name" in dic:
-			message = "Добрий день, " + dic["name"] + "!"
+			message = "Hello, " + dic["name"] + "!"
 		else:
-			message = "Добрий день, stranger!"
+			message = "Hello, stranger!"
+
+		
+		
+
+		message = 'USDT / UAH';
+		message += '
+		'
+		print('USDT / UAH');
+		print('');
+
+		#Binance
+
+		response = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=USDTUAH")
+		#print(response.status_code)
+		#print(response.json())
+
+		binance_price = response.json()['price']
+		print('Binance: ' + "%.2f" % float(binance_price) + ' грн')
+
+		#Huobi
+
+		response = requests.get("https://api.huobi.pro/market/detail/merged?symbol=usdtuah")
+
+		huobi_price_close = response.json()['tick']['close']
+		huobi_price_bid = response.json()['tick']['bid']
+		huobi_price_ask = response.json()['tick']['ask']
+
+		print('');
+		print('Huobi close закриття: ' + "%.2f" % float(huobi_price_close) + ' грн')
+		print('Huobi bid купівля: ' + "%.2f" % float(huobi_price_bid[0]) + ' грн')
+		print('Huobi ask продаж: ' + "%.2f" % float(huobi_price_ask[0]) + ' грн')
+
+		#KUNA
+
+		response = requests.get("https://api.kuna.io//v3/tickers?symbols=usdtuah")
+		kuna = response.json()
+
+		print('');
+		print('KUNA close: ' + "%.2f" % float(kuna[0][7]) + ' грн')
+		print('KUNA bid: ' + "%.2f" % float(kuna[0][1]) + ' грн')
+		print('KUNA ask: ' + "%.2f" % float(kuna[0][3]) + ' грн')
+
+
+
 
 		self.wfile.write(message.encode())
+
+
+
+
 		return
